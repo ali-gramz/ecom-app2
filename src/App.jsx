fd4b067491b506d811233ce0815af1f6505b2992
ali-gramz <gramzali708@gmail.com>
2026-03-10 11:55:11 +0000

    last2 commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
 import Header from "./components/Header";
-import style from"./App.module.css";
+import style from "./App.module.css";
 import Footer from "./components/Footer";
 import Home from "./section/Home";
 import AboutUs from "./section/AboutUs";
 import Contact from "./section/Contact";
 import Products from "./section/Products";
 
-
-export default function App(){
-  return(
+export default function App() {
+  return (
     <>
-     <Header/>
-     <main>
-        <Home/>
-        <Products/>
-        <AboutUs/>
-        <Contact/>
-     </main>
-     <Footer/>
+      <Header />
+      <main>
+        <Home />
+        <Products />
+        <AboutUs />
+        <Contact />
+      </main>
+      <Footer />
     </>
-  )
+  );
 }
